b16cd137a56e4879d5e5bb51eb14a87d06df7e00
Abdul-Muiz Yusuff <muiz.rpi@gmail.com>
2021-01-21 14:34:36 -0500

    Created LeaseDocument model for storing lease documents on landlord accounts

--- GQL/resolvers/LeaseDocumentResolver.ts
+++ GQL/resolvers/LeaseDocumentResolver.ts
@@ -1,13 +1,38 @@
 import {Resolver, Mutation, Arg, Query} from 'type-graphql'
 import {DocumentType} from '@typegoose/typegoose'
 import {Landlord, LandlordModel} from '../entities/Landlord'
-import {LeaseDocument, LeaseDocumentModel, LeaseDocumentAPIResponse} from '../entities/LeaseDocument'
+import {LeaseDocument, LeaseDocumentModel,
+    MultipleLeaseDocumentsAPIResponse,
+    LeaseDocumentAPIResponse} from '../entities/LeaseDocument'
 import mongoose, {DocumentQuery} from 'mongoose'
 
 const ObjectId = mongoose.Types.ObjectId;
 
 @Resolver()
 export class LeaseDocumentResolver {
+
+    @Query(returns => MultipleLeaseDocumentsAPIResponse)
+    async getLeaseDocumentsForLandlord(
+        @Arg("landlord_id") landlord_id: string
+    ): Promise<MultipleLeaseDocumentsAPIResponse>
+    {
+
+        if (ObjectId.isValid(landlord_id)) {
+            return {
+                success: false,
+                error: "Invalid landlord id."
+            }
+        }
+
+        let lease_documents: DocumentType<LeaseDocument>[] = await LeaseDocumentModel.find({landlord_id}) as DocumentType<LeaseDocument>[];
+        return {
+            success: true,
+            data: {
+                lease_documents
+            }
+        }
+
+    }
 
     @Mutation(returns => LeaseDocumentAPIResponse)
     async addNewLeaseDocument(
