eb9db6c50acdfe89edd1c74cae8722ce8ea2e5dd
Josh Malek <17jmalek98@gmail.com>
2021-03-03 04:27:20 -0500

    add tqdm progress and image params

--- image_suggest/main.py
+++ image_suggest/main.py
@@ -1,19 +1,30 @@
 import requests
 import os
+from tqdm import tqdm
 from dotenv import load_dotenv
 load_dotenv()
 
 API_KEY = os.getenv("GCP_API")
 api_url = "https://maps.googleapis.com/maps/api/streetview?"
-size = "size=600x600"
+size = "size=665x441"
 inp_address = input("What address do you want: ")
 address = "&location=" + inp_address.replace(" ", "+")
-print(API_KEY)
+#print(API_KEY)
 image_request = api_url+size+address+"&key="+API_KEY
-print(image_request)
+#print(image_request)
 
-response = requests.get(image_request)
-image_path = inp_address.replace(" ","_")
-file = open("images/sample_image.png", "wb")
-file.write(response.content)
-file.close()
+response = requests.get(image_request, stream=True)
+total_size_in_bytes= int(response.headers.get('content-length', 0))
+block_size = 1024 #1 Kibibyte
+progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+
+image_path = "images/" + inp_address.replace(" ","_")+".png"
+
+with open(image_path, 'wb') as file:
+    for data in response.iter_content(block_size):
+        progress_bar.update(len(data))
+        file.write(data)
+progress_bar.close()
+if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+    print("ERROR, something went wrong")
+
